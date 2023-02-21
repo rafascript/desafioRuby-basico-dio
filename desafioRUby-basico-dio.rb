87c0ce211eb892b30af89cc7374ba4a1c87ee58f
rafascript <rafa.eel1@live.com>
2023-02-21 01:24:26 -0300

    first commit

---
+++ desafioRUby-basico-dio.rb
@@ -0,0 +1,8 @@
+print("Digite seu nome: ")
+nome = gets.chomp
+print("Digite seu sobrenome: ")
+sobrenome = gets.chomp
+print("Digite sua idade: ")
+idade = gets.chomp
+
+puts "Olá #{nome} #{sobrenome}! Seja bem-vindo à biblioteca da DIO! Sua idade é #{idade}"
